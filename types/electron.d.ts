f0196c0cd45ad6b7a559ebf73df3369d8f8a457e
REV_PRO <rev_pro@REV-PROui-MacBookPro.local>
2026-01-21 14:13:57 +0900

    fix: ElectronAPI onLogout 타입 추가

--- types/electron.d.ts
+++ types/electron.d.ts
@@ -4,12 +4,14 @@ declare global {
   interface Window {
     electronAPI?: {
       isElectron: boolean
-      openMessengerApp: () => void
+      toggleMessenger: () => void
+      openChat: (roomId: string, roomName: string) => void
+      closeChat: (roomId: string) => void
       closeWindow: () => void
       minimizeWindow: () => void
+      onLogout: () => void
+      onLogin: () => void
       showNotification: (title: string, body: string) => void
-      selectFile: () => Promise<string | null>
-      openPath: (filePath: string) => void
     }
   }
 }
